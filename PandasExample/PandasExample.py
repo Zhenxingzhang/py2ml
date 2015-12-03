4b5541cb216d03c17ca64e49730fec4d000ffc57
Zhenxing Zhang <Zhenxingzhang@users.noreply.github.com>
2015-12-03 22:39:15 +0000

    Add dataframe example

--- PandasExample/PandasExample.py
+++ PandasExample/PandasExample.py
@@ -4,7 +4,20 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
+# Series
+
 s = pd.Series([1, 3, 5 ,np.nan, 6, 8])
 
 print type(s), s.dtype, s.index, s[0]
 
+# Dataframe
+
+d = {'one': pd.Series([1,2,3],index=['a','b','c']),
+     'two': pd.Series([1,2,3,4], index=['a','b','c','d'])
+     }
+
+print type(d)
+
+df = pd.DataFrame(d)
+
+iris = pd.read_csv('data/iris.data')
