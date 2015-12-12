8cdd4070642eb0c4e5bff5bad895e35b10fb2a8f
zhenxing zhang <zhenxing@zhenxings-MacBook-Pro.local>
2015-12-12 01:22:32 +0000

    Add sklearn example

--- NeuralNetwork/IntuitionsExample.py
+++ NeuralNetwork/IntuitionsExample.py
@@ -1,5 +1,7 @@
 __author__ = 'zhenxing'
 
+import matplotlib.pyplot as plt
+import pandas as pd
 from sklearn.neural_network import MLPClassifier
 
 # X=[ [1,0,0], [1,0,1] ,[1,1,0] , [1,1,1]]
@@ -19,19 +21,28 @@ digits = datasets.load_digits()
 X_digits = digits.data
 y_digits = digits.target
 
-n_neighbors = 5
+print type(X_digits), X_digits.shape
+#
+# n_neighbors = 5
 # alg = neighbors.KNeighborsClassifier(n_neighbors, weights="distance")
+#
+alg = LogisticRegression(C=1e1, max_iter=100)
 
-# alg = LogisticRegression(C=1e5)
-
-# alg = svm.SVC(C=1, kernel="linear")
-
-# alg = MLPClassifier(activation = 'logistic', algorithm='l-bfgs', alpha=1e-5, hidden_layer_sizes=(100,20), random_state=1)
-
+# # alg = svm.SVC(C=1, kernel="linear")
+#
 kfold = 3
-
-
-
 scores = cross_validation.cross_val_score(alg, X_digits, y_digits, cv=kfold, n_jobs= -1)
-
 print scores.mean()
+
+# scores = []
+# hidden_neurals = [idx for idx in range(1, 80, 5)]
+# for hidden_neural in hidden_neurals:
+#     alg = MLPClassifier(activation = 'logistic', algorithm='l-bfgs', alpha=1e-5, hidden_layer_sizes=(hidden_neural), random_state=1)
+#     cross_scores = cross_validation.cross_val_score(alg, X_digits, y_digits, cv=kfold, n_jobs= -1)
+#     scores.append(cross_scores.mean())
+# print scores
+#
+# plt.scatter(hidden_neurals, scores)
+# plt.xlabel("Number of hidden neurals")
+# plt.ylabel("Cross validation score")
+# plt.show()
